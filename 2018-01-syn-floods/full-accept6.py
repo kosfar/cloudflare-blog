7decc3f702cede78a1f98fdd80ee30183234d74c
Kostis Fardelas <dante1234@gmail.com>
2020-05-31 23:42:00 +0300

    Emulate full accept q for ipv6

---
+++ 2018-01-syn-floods/full-accept6.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import socket
+import os
+
+PORT = 1235
+BACKLOG = 2
+QSIZE = 10
+
+s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+s.bind(('::', PORT))
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# s.setsockopt(socket.IPPROTO_TCP, socket.TCP_DEFER_ACCEPT, 120)
+s.listen(BACKLOG)
+
+# Filling Accept Queue with unhandled connection attempts, to imitate
+# slow application.
+
+C = []
+
+for i in range(QSIZE + 2):
+    if i == QSIZE + 1:
+        os.system("nstat -r > /dev/null")
+    c = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    c.setblocking(False)
+    try:
+        c.connect(('::1', PORT))
+    except socket.error:
+        pass
+    C.append(c)
+    if i == QSIZE + 1:
+        os.system("nstat")
+
